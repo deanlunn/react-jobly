be1c154915cd046856e462d15ce2f85a31689be4
Dean Lunn <dean.lunn82@gmail.com>
2023-08-17 11:57:27 -0600

    third commit

--- backend/jobly.sql
+++ backend/jobly.sql
@@ -1,9 +1,9 @@
-\echo 'Delete and recreate jobly2 db?'
+\echo 'Delete and recreate jobly db?'
 \prompt 'Return for yes or control-C to cancel > ' foo
 
-DROP DATABASE jobly2;
-CREATE DATABASE jobly2;
-\connect jobly2
+DROP DATABASE jobly;
+CREATE DATABASE jobly;
+\connect jobly
 
 \i jobly-schema.sql
 \i jobly-seed.sql
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -23,7 +23,7 @@ router.post("/token", async function (req, res, next) {
   try {
     const validator = jsonschema.validate(req.body, userAuthSchema);
     if (!validator.valid) {
-      const errs = validator.errors.map((e) => e.stack);
+      const errs = validator.errors.map(e => e.stack);
       throw new BadRequestError(errs);
     }
 
@@ -35,6 +35,7 @@ router.post("/token", async function (req, res, next) {
     return next(err);
   }
 });
+
 
 /** POST /auth/register:   { user } => { token }
  *
@@ -49,7 +50,7 @@ router.post("/register", async function (req, res, next) {
   try {
     const validator = jsonschema.validate(req.body, userRegisterSchema);
     if (!validator.valid) {
-      const errs = validator.errors.map((e) => e.stack);
+      const errs = validator.errors.map(e => e.stack);
       throw new BadRequestError(errs);
     }
 
@@ -60,5 +61,6 @@ router.post("/register", async function (req, res, next) {
     return next(err);
   }
 });
+
 
 module.exports = router;
