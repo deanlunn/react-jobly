9e2a640d5145aef310d6ef33f1ba358ee5c333e3
Dean Lunn <dean.lunn82@gmail.com>
2023-08-17 13:06:06 -0600

    build commit

---
+++ frontend/build/precache-manifest.c941e028265cfd8db4362cd1eaf4c6c6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ebe26dc03cfda704d0dd4e11af8d269",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c310f7da1d5228c9c0ac",
+    "url": "/static/css/2.560b9325.chunk.css"
+  },
+  {
+    "revision": "75ca62660486cc371597",
+    "url": "/static/css/main.658eb3e8.chunk.css"
+  },
+  {
+    "revision": "c310f7da1d5228c9c0ac",
+    "url": "/static/js/2.fd807209.chunk.js"
+  },
+  {
+    "revision": "395cc4b3602716a6dc8075912d30f948",
+    "url": "/static/js/2.fd807209.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75ca62660486cc371597",
+    "url": "/static/js/main.94cc7de5.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.94cc7de5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e157b3318261263bb32a",
+    "url": "/static/js/runtime-main.ca09d11f.js"
+  }
+]);
