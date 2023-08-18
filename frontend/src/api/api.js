088f99a27c6bc06fbedb012b541d88715e6a3709
Dean Lunn <dean.lunn82@gmail.com>
2023-08-18 16:29:17 -0600

    db fix commit 2

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,8 +1,6 @@
 import axios from "axios";
 
-const BASE_URL =
-  process.env.REACT_APP_BASE_URL ||
-  "https://react-jobly-backend-0cnx.onrender.com";
+const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:3001";
 
 /** API Class.
  *
